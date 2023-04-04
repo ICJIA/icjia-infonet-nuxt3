5722e7eb5c19c5b2293e42acdb4044951c125ec1
cschweda <cschweda@gmail.com>
2023-04-04 09:52:06 -0500

    fix: optimize context footer

--- components/content/TheContextFooter.vue
+++ components/content/TheContextFooter.vue
@@ -1,50 +1,51 @@
 <template>
   <nav>
-    <div
-      variant="text"
-      class="text-center pt-3 pb-3 footer-bg"
-      style="justify-content: space-around"
-    >
-      <span v-for="(item, index) in navMenu" :key="index">
-        <NuxtLink
-          :to="item.link"
-          class="context-link px-2"
-          style="font-size: 14px; font-family: 'Roboto', sans-serif !important"
-          >{{ item.main }}</NuxtLink
-        >
+    <div class="nav-footer">
+      <span v-for="(item, index) in navMenu" :key="index" class="nav-item">
+        <NuxtLink :to="item.link" class="context-link">{{
+          item.main
+        }}</NuxtLink>
         <span v-if="menuLength - 1 !== index"> | </span>
       </span>
     </div>
   </nav>
 </template>
-
 <script setup>
 const appConfig = useAppConfig();
-const navMenu = JSON.parse(JSON.stringify(appConfig.footerMenu));
+const navMenu = appConfig.footerMenu;
 const menuLength = navMenu.length;
 </script>
-
 <style scoped>
-a.context-link {
-  font-family: "Raleway" !important;
-  color: #fff;
-  font-size: 10px;
-  font-weight: 700;
-  text-decoration: none;
-
-  text-transform: uppercase;
-  letter-spacing: 0.09em;
-}
-
-a.context-link:hover {
-  /* text-decoration: underline; */
-  color: #aaa;
-}
-.footer-bg {
+.nav-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: #0d4494;
   min-width: 100%;
   color: #fff;
   font-size: 16px;
   font-weight: 900;
+  padding: 1rem 0;
+  text-align: center;
+}
+
+.nav-item {
+  display: inline-flex;
+  align-items: center;
+}
+
+.context-link {
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+  color: #fff;
+  font-weight: 700;
+  text-decoration: none;
+  text-transform: uppercase;
+  letter-spacing: 0.09em;
+  padding: 0 0.5rem;
+}
+
+.context-link:hover {
+  color: #aaa;
 }
 </style>
