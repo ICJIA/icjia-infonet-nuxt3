ad295835f58f82afa338ecc27af8a43b58486e80
cschweda <cschweda@gmail.com>
2023-04-12 07:37:18 -0500

    fix: slug

--- pages/news/[slug].vue
+++ pages/news/[slug].vue
@@ -86,7 +86,7 @@ useHead({
     {
       hid: "og-image",
       property: "og:image",
-      content: "https://dvfr.icjia.dev/dvfr-splash-text-01.jpg",
+      content: "https://infonet.icjia.dev/icjia-logo.png",
     },
     {
       hid: "og-image-width",
