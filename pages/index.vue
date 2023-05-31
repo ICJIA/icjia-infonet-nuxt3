a3e3006f1ef74afb980845cd74b60189ae8ed7a3
cschweda <cschweda@gmail.com>
2023-05-31 15:03:13 -0500

    fix: infonet hub card

--- pages/index.vue
+++ pages/index.vue
@@ -8,25 +8,41 @@
       style="margin-bottom: -45px !important; margin-top: -20px"
       class="py-5"
     >
+      <h1>Infonet Home content here</h1>
       <ContentDoc :key="data?.title" :value="data" class="markdown-body">
         <template #empty>Document not found</template>
         <template #not-found>Document not found</template>
       </ContentDoc>
     </div>
-    <h2 class="mt-12">Latest Infonet ResearchHub articles (test query):</h2>
-    <div v-for="article in articles" :key="article._id">
-      <v-card elevation="5" class="mt-8 px-10 py-10">
-        <div>{{ formatDate(article.date) }}</div>
-        <h3>{{ article.title }}</h3>
-        <h4>
-          <span v-for="(author, index) in article.authors" :key="author.title">
-            {{ author.title
-            }}<span v-if="index < article.authors.length - 1">, </span>
-          </span>
-        </h4>
-        <div>{{ article.abstract }}</div>
-        <v-img width="100%" :src="article.splash"></v-img>
-      </v-card>
+    <h2 class="mt-12">
+      Latest Infonet ResearchHub articles (this is a test -- do not be alarmed):
+    </h2>
+    <div class="px-12">
+      <div v-for="article in articles" :key="article._id">
+        <v-card
+          elevation="5"
+          class="mt-8 px-10 py-10 info-card"
+          @click="gotoArticle(article.slug)"
+        >
+          <div>{{ formatDate(article.date) }}</div>
+          <h3>{{ article.title }}</h3>
+          <h4>
+            <span
+              v-for="(author, index) in article.authors"
+              :key="author.title"
+            >
+              {{ author.title
+              }}<span v-if="index < article.authors.length - 1">, </span>
+            </span>
+          </h4>
+          <div>{{ article.abstract }}</div>
+          <h5 class="text-center">Splash image:</h5>
+
+          <div class="text-center mt-8">
+            <img :src="article.splash" style="width: 30%" />
+          </div>
+        </v-card>
+      </div>
     </div>
   </div>
 </template>
@@ -50,6 +66,11 @@ const { data } = await useAsyncData(`content-home`, async () => {
 onMounted(() => {
   isMounted.value = true;
 });
+
+const gotoArticle = (slug) => {
+  let hubArticle = `https://icjia.illinois.gov/`;
+  return window.open(hubArticle, "_blank");
+};
 
 const formatDate = (dateString) => {
   const options = { year: "numeric", month: "long", day: "numeric" };
