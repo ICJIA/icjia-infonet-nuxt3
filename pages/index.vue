03cfa4f898f8e8b9040afc3d692d683dfa89c8dc
cschweda <cschweda@gmail.com>
2023-06-01 10:11:14 -0500

    fix: hub test slides (check for mobile via useMobile)

--- pages/index.vue
+++ pages/index.vue
@@ -15,47 +15,12 @@
       </ContentDoc>
     </div>
 
-    <!-- <v-container
-      ><v-row
-        ><v-col cols="12">
-          <h2 class="mt-12">
-            Latest Infonet ResearchHub articles (this is a only a test -- do not
-            be alarmed):
-          </h2>
-
-          <div v-for="article in articles" :key="article._id">
-            <v-card
-              elevation="5"
-              class="mt-8 px-10 py-10 info-card"
-              @click="gotoArticle(article.slug)"
-            >
-              <div>{{ formatDate(article.date) }}</div>
-              <h3>{{ article.title }}</h3>
-              <h4>
-                <span
-                  v-for="(author, index) in article.authors"
-                  :key="author.title"
-                >
-                  {{ author.title
-                  }}<span v-if="index < article.authors.length - 1">, </span>
-                </span>
-              </h4>
-              <div>{{ article.abstract }}</div>
-              <h5 class="text-center">Tags:</h5>
-
-              <div class="text-center">
-                {{ article.tags }}
-              </div>
-              <h5 class="text-center">Splash image:</h5>
-
-              <div class="mt-8">
-                <v-img :src="article.splash" width="50%" class="mx-auto" />
-              </div>
-            </v-card>
-          </div> </v-col></v-row
-    ></v-container> -->
     <h2 class="mt-12 px-10">Latest Infonet ResearchHub articles (test):</h2>
-    <v-sheet class="mx-auto mt-12" elevation="8">
+    Mobile: {{ mobile }}
+    <span v-if="!mobile"
+      >(To see mobile view, please reduce width of browser)</span
+    >
+    <v-sheet class="mx-auto mt-12" elevation="8" v-if="!mobile">
       <v-slide-group
         v-model="model"
         class="pa-4"
@@ -104,21 +69,55 @@
           </v-card>
         </v-slide-group-item>
       </v-slide-group>
-
-      <v-expand-transition>
-        <v-sheet v-if="model != null" height="200">
-          <div class="d-flex fill-height align-center justify-center">
-            <h3 class="text-h6">Selected {{ model }}</h3>
-          </div>
-        </v-sheet>
-      </v-expand-transition>
     </v-sheet>
+    <div v-else>
+      <h2>Hub articles for mobile here</h2>
+      <div v-for="article in articles" :key="article._id">
+        <v-card
+          elevation="2"
+          class="mx-3 px-3 py-5 info-card"
+          outlined
+          min-width="500"
+          min-height="200"
+          style="width: 100%"
+          @click="gotoArticle(article.slug)"
+        >
+          <div>
+            {{ formatDate(article.date) }}
+          </div>
+          <div class="my-6" style="font-weight: 900">
+            {{ article.title }}
+          </div>
+          <v-img
+            :src="article.splash"
+            cover
+            height="200"
+            class="mb-5"
+            :ref="'img_' + article._id"
+            style="border: 1px solid #fafafa"
+            alt="ICJIA News image"
+            ><template #placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="blue darken-3"
+                  aria-label="progress"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+          <div style="max-width: 500px">{{ article.abstract }}</div>
+        </v-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import hubArticles from "~/assets/json/hub.json";
-// const { path } = useRoute();
+import { useDisplay } from "vuetify";
+const { mobile } = useDisplay();
+const isMobile = ref(mobile);
 // const router = useRouter();
 const isMounted = ref(false);
 const articles = ref(hubArticles);
