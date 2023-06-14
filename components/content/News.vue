43522b848bbad0ebd7a232583342ff5f8c8f95a4
cschweda <cschweda@gmail.com>
2023-06-14 08:17:49 -0500

    fix: news card

--- components/content/News.vue
+++ components/content/News.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="i in 3" :key="i" class="mb-4 info-card">
-      <v-card height="200px" class="px-5 py-5">
+      <v-card height="200px" class="px-5 py-5" elevation="0">
         <b>News item {{ i }} here</b></v-card
       >
     </div>
