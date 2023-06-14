afefcd6e6bc34a300e480895262a55ed905a993e
cschweda <cschweda@gmail.com>
2023-06-14 12:04:04 -0500

    fix: news display on home

--- components/content/News.vue
+++ components/content/News.vue
@@ -1,13 +1,45 @@
 <template>
   <div>
     <div v-for="i in 3" :key="i" class="mb-4 info-card">
-      <v-card height="150px" class="px-5 py-5" elevation="0">
-        <b>News item {{ i }} here</b></v-card
-      >
+      <v-card color="#fff" theme="dark" class="px-5 py-3" max-height="350">
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <v-avatar class="hidden-sm-and-down" size="150" rounded="0">
+            <v-img
+              src="/news-square.jpg"
+              style="min-height: 100% !important; min-width: 100% !important"
+              cover
+            ></v-img>
+          </v-avatar>
+          <div class="px-5" style="color: #000; margin-top: -20px">
+            <h3>Lorem markdownum peragant</h3>
+            <div
+              style="
+                margin-top: -15px;
+                margin-bottom: 20px;
+                color: #222;
+                font-size: 14px;
+              "
+            >
+              {{ formatDate(new Date()) }}
+            </div>
+            Lorem markdownum nigraque peragant mentis. Ascaniumque bella
+            excussis horrida concurrere est qua Romane magnorum ursos equa
+            cuspis binominis fuit. Quod modo, quod tenax: raro evinctus
+            superando color annos dicta, Cephalus more, est? Letalem quamvis
+            puer gruem, ante leniat nisi collige membra, sic timemus resonant in
+            pacem ad tibi illa.
+          </div>
+        </div>
+      </v-card>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
+</script>
 
 <style lang="scss" scoped></style>
