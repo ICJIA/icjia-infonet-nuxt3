25b869a52b7de99b3f537348cc60777b5511767d
cschweda <cschweda@gmail.com>
2023-06-14 07:29:49 -0500

    fix: home layout for faqs and news

--- components/content/Faqs.vue
+++ components/content/Faqs.vue
@@ -3,7 +3,7 @@
     <div v-for="i in 10" :key="i" class="mb-4">
       <v-expansion-panels>
         <v-expansion-panel
-          title="Item"
+          :title="`FAQ Item ${i}`"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         ></v-expansion-panel>
       </v-expansion-panels>
--- components/content/News.vue
+++ components/content/News.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div v-for="i in 3" :key="i" class="mb-4 info-card">
-      <v-card height="200px" class="px-5 py-5"> <b>News item here</b></v-card>
+      <v-card height="200px" class="px-5 py-5">
+        <b>News item {{ i }} here</b></v-card
+      >
     </div>
   </div>
 </template>
