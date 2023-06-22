ad95fdb8bbed9aab4cb12ec84a7d16f31ee2f1b7
cschweda <cschweda@gmail.com>
2023-06-22 15:33:31 -0500

    fix: temp remove loader

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app id="appTop">
-    <div style="margin-top: 150px" class="text-center" v-if="!isMounted">
+    <!-- <div style="margin-top: 150px" class="text-center" v-if="!isMounted">
       <div class="text-center">
         <div style="font-weight: 700; font-size: 25px; margin-bottom: 25px">
           LOADING
         </div>
         <img src="/home-loading.gif" />
       </div>
-    </div>
+    </div> -->
     <TheNav></TheNav>
 
     <TheSidebar></TheSidebar>
