231825dc22b717c5defd3fef7b9a00b9d806b84e
cschweda <cschweda@gmail.com>
2023-06-24 09:12:54 -0500

    fix: fonts

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,13 +2,24 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as labs from "vuetify/labs/components";
 
-import "@mdi/font/css/materialdesignicons.css";
+// import "@mdi/font/css/materialdesignicons.css";
+// import "font-awesome/css/font-awesome.min.css";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import { mdi } from "vuetify/iconsets/mdi";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     // ssr: true,
+    // icons: {
+    //   iconfont: ["mdiSvg", "fa", "mdi", "fa4"], // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    // },
     icons: {
-      iconfont: ["mdiSvg", "fa", "mdi", "fa4"], // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+      defaultSet: "mdi",
+      aliases,
+      sets: {
+        mdi,
+        fa,
+      },
     },
     components: {
       ...components,
