7fcc361174942aed5cea56ccb2f5db5681924a24
cschweda <cschweda@gmail.com>
2023-07-01 08:51:08 -0500

    fix: default icons for vuetify

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,28 +1,15 @@
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
+// import * as components from "vuetify/components";
+// import * as directives from "vuetify/directives";
 import * as labs from "vuetify/labs/components";
-
-// import "@mdi/font/css/materialdesignicons.css";
-// import "font-awesome/css/font-awesome.min.css";
-import { aliases, fa } from "vuetify/iconsets/fa";
-import { mdi } from "vuetify/iconsets/mdi";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     // ssr: true,
-    // icons: {
-    //   iconfont: ["mdiSvg", "fa", "mdi", "fa4"], // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
-    // },
     icons: {
-      defaultSet: "mdi",
-      aliases,
-      sets: {
-        mdi,
-        fa,
-      },
+      iconfont: ["mdiSvg", "fa"], // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
     },
     components: {
-      ...components,
       ...labs,
     },
   });
